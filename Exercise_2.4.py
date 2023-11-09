4ad23b53b0c39110969ac9d556eff85bd1789858
ErnoCso <147259999+ErnoCso@users.noreply.github.com>
2023-11-09 12:00:21 +0000

    Update Exercise_2.4.py

--- Exercise_2.4.py
+++ Exercise_2.4.py
@@ -10,3 +10,17 @@ elif sweets // pupils == 1:
     print(f"Each pupil gets {sweets // pupils} sweet and has {sweets % pupils} left over ")
 else:
     print(f"Each pupil gets {sweets // pupils} sweets and has {sweets % pupils} left over ")
+
+
+
+
+
+
+if __name__ == '__main__':
+    pizza = 12
+    tuesday_pizza = 0.5
+    delivery_cost = 2.50
+    bpp_app_orders = 0.25
+    pizza_order = int(input(" How many pizza ordered? "))
+    delivery = input("Is delivery required? Y/N")
+    tuesday = input("Is it Tuesday?")
